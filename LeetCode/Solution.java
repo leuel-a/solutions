9c03d8ee524fd1aea9a82051f78e31a29fb7f37c
leuel-a <leuel.asfaw@gmail.com>
2026-09-24 15:14:40 +0300

    add solution for 160_Intersection_of_Two_Linked_Lists

--- LeetCode/Solution.java
+++ LeetCode/Solution.java
@@ -1,5 +1,5 @@
-class Solution {
+public class Solution {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World!");
     }
 }
